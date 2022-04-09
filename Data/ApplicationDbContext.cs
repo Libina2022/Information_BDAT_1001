31f2e27fa5689f3f13d0722e919b40ccadff7f99
Libina2022 <200508120@student.georgianc.on.ca>
2022-04-09 19:06:42 -0400

    latest changes

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContactManager_BDAT1001.Models;
+
+namespace ContactManager_BDAT1001.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<ContactManager_BDAT1001.Models.Contact> Contact { get; set; }
+}
